5e376e9987a49e21a8f622f48af09af21fe61d61
Pavel Zhelihovskiy <neckazhyua@gmail.com>
2024-01-25 14:15:00 +0200

    Last

--- src/pages/Frame1.js
+++ src/pages/Frame1.js
@@ -13,17 +13,19 @@ const Frame1 = () => {
       <input type="password" className="b" placeholder="Пароль" />
 
       <div className="div1">
+        {/* Добавьте ваш код для div1 */}
       </div>
       <div className="item" />
       <div className="child" />
  
       <b className="b1">Авторизація</b>
-      <Link to ="/register">
-       <button className="b3">Немає аккаунта?</button>
-      </Link>
+      <button className="b3">Немає аккаунта?</button>
+,
       <div className="div2">
         <div className="div3" />
+          <Link to ="/register">
           <button className="b2">Продовжити</button>
+          </Link>
       </div>
 
       <div className="div4">
